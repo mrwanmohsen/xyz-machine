6bf756de3ad38fd7f931855833985e9b7acde18c
Mrwan Mohsen <mrwanmohsen@Mrwans-Mac.local>
2019-02-12 13:58:10 +0200

    here we begin

---
+++ xyz-machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Mrwan Mohsen on 2/12/19.
+//  Copyright © 2019 Mrwan Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
